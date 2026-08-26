36fa903db76d4c99a857ead8e8e88dc332ae18d8
Ziad Gad <ziadamrjad@gmail.com>
2026-08-26 15:59:35 +0300

    pushing what i have learned about ref and out keywords

---
+++ DealingWithCSharp/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace DealingWithCSharp;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
---
+++ RefAndOutKeywords/Program.cs
@@ -0,0 +1,83 @@
+﻿namespace RefAndOutKeywords;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        TestValue();
+        TestRef();
+        TestOut();
+    }
+
+
+    public static void TestValue()
+    {
+        bool isSuccessful = true;
+        var resault = Divide(10, 0, isSuccessful);
+        Console.WriteLine($"isSuccessful = {isSuccessful}");
+        Console.WriteLine($"resault = {resault}");
+    }
+
+    /// <remarks>
+    /// ref parameter MUST be initialized before use.
+    /// bool isSuccessful; Error!!
+    /// </remarks>
+    public static void TestRef()
+    {
+        bool isSuccessful = true;
+        var resault = DivideRef(10, 0, ref isSuccessful);
+        Console.WriteLine($"isSuccessful = {isSuccessful}");
+        Console.WriteLine($"resault = {resault}");
+    }
+
+    /// <remarks>
+    /// The out parameter doesn't need to be initialized before use.
+    /// However, you must assign a value to it before returning from the method.
+    /// </remarks>
+    public static void TestOut()
+    {
+        bool isSuccessful;
+        var resault = DivideOut(10, 0, out isSuccessful);
+        Console.WriteLine($"isSuccessful = {isSuccessful}");
+        Console.WriteLine($"resault = {resault}");
+    }
+
+    static double DivideOut(double number, double divisor, out bool isSuccessful)
+    {
+        if (divisor == 0)
+        {
+            Console.WriteLine($"Cannot divide by zero!");
+            isSuccessful = false; // if you don't assign a value, you will get an error.
+            return 0;
+        }
+
+        isSuccessful = true;      // if you don't assign a value you will get an error.
+        return number / divisor;
+    }
+
+    static double DivideRef(double number, double divisor, ref bool isSuccessful)
+    {
+        if (divisor == 0)
+        {
+            Console.WriteLine($"Cannot divide by zero!");
+            isSuccessful = false;
+            return 0;
+        }
+
+        isSuccessful = true;
+        return number / divisor;
+    }
+
+    static double Divide(double number, double divisor, bool isSuccessful)
+    {
+        if (divisor == 0)
+        {
+            Console.WriteLine($"Cannot divide by zero!");
+            isSuccessful = false;
+            return 0;
+        }
+
+        isSuccessful = true;
+        return number / divisor;
+    }
+}
