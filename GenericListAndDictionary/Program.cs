ed60bde0c43f0bc2b73c2018fb3a48e5d57cfaf1
Ziad Gad <ziadamrjad@gmail.com>
2026-09-22 10:01:27 +0300

    Pusing what i have learned about Generic List

---
+++ GenericListAndDictionary/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace GenericListAndDictionary;
+
+class Program
+{
+    /**
+     * Generic List is the generic equivalent for the ArrayList
+     * Generic List solves the boxing and unboxing problem, because it's a strong typed.
+     * Strong Typed like Array
+     * Dynamic like ArrayList
+     * Same Array and ArrayList built-in functions.
+     */
+    static void Main(string[] args)
+    {
+        var list = new List<int>();
+        list.Add(1);
+        list.Add(2);
+        // list.Add("ziad"); // Error: Argument type 'string' is not assignable to parameter type 'int'
+        
+    }
+}
