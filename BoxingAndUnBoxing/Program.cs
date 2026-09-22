0624f815e5eba4ffdbd09fc522f82b9ee849ce40
Ziad Gad <ziadamrjad@gmail.com>
2026-09-22 09:53:38 +0300

    pushing what i have learned about boxing and unboxing

---
+++ BoxingAndUnBoxing/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace BoxingAndUnBoxing;
+
+class Program
+{
+    /**
+     * Boxing is to cover the value type with a reference type to save it in the heap memory.
+     *      - After Boxing you can't use it like how it was, you can't add to it or subtract from it.
+     *      - First you have to UnBox it.
+     * UnBoxing is to return it as it was.
+     */
+    static void Main(string[] args)
+    {
+        // Boxing
+        int x = 5; // value type
+        object y = x; // reference type
+        Console.WriteLine(x); // -> 5
+        Console.WriteLine(y); // -> 5
+
+        //int z = y + x; // Error: Cannot apply operator '+' to operands of type 'object' and 'int'
+        
+        // UnBoxing with typecase
+        int z = (int)y + x;
+        Console.WriteLine(z);
+
+
+
+    }
+}
