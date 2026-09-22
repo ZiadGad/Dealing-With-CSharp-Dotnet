8c2f735f2a83f3fe7d71a9ded376e95f3b1b9e68
Ziad Gad <ziadamrjad@gmail.com>
2026-09-22 10:07:53 +0300

    pushing what i have learned about dicionary

--- GenericListAndDictionary/Program.cs
+++ GenericListAndDictionary/Program.cs
@@ -8,13 +8,33 @@ class Program
      * Strong Typed like Array
      * Dynamic like ArrayList
      * Same Array and ArrayList built-in functions.
+     *
+     * Dictionary
      */
     static void Main(string[] args)
     {
+        //  Generic List
         var list = new List<int>();
         list.Add(1);
         list.Add(2);
         // list.Add("ziad"); // Error: Argument type 'string' is not assignable to parameter type 'int'
+        
+        // Dictionary
+        var emails = new Dictionary<string, string>();
+        emails.Add("gmail","ziadamrjad@gmail.com");
+        emails.Add("icloud","ziiadamrjad@icloud.com");
+        
+        if(!emails.ContainsKey("icloud"))
+            emails.Add("icloud","ziiadamrjad@icloud.com");
+        
+        //Console.WriteLine(emails["hotmail"]); // Error Crashes: The given key 'hotmail' was not present in the dictionary.
+        
+        if(emails.ContainsKey("hotmail")) // Safely Print
+            Console.WriteLine(emails["hotmail"]);
+        
+        if(emails.TryGetValue("hotmail", out string value)) // Another Safely Print
+            Console.WriteLine(value);
+
         
     }
 }
